26e5d2c8adbd3f02d07370ef4271534a75f72042
KonradSmuga <smuggk@gmail.com>
2018-11-21 13:53:08 +0100

    average random numbers

---
+++ src/main/java/Average.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class Average {
+
+    public void average() {
+        int numbers = 50;
+
+        int[] array = new int[numbers];
+        int randomNumbers = 100;
+        Random random = new Random(randomNumbers);
+
+        for (int i = 0; i < numbers; i++)
+
+            array[i] = random.nextInt(numbers);
+        double sum =0;
+
+        for(int i = 0; i < numbers; i++){
+            System.out.println(array[i]);
+
+            sum += array[i];
+        }
+
+        System.out.println(sum/numbers);
+    }
+
+
+
+}
+
