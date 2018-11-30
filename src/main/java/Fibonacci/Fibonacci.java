5931b7ced1d7f3a3f0ae25206a7bfefaf4173aef
KonradSmuga <smuggk@gmail.com>
2018-11-30 13:04:21 +0100

    fibonacci v2

--- src/main/java/Fibonacci/Fibonacci.java
+++ src/main/java/Fibonacci/Fibonacci.java
@@ -1,13 +1,12 @@
 package Fibonacci;
 
-import static Fibonacci.Fibonacci.fibonacci;
 
 public class Fibonacci {
-    public static long fibonacci(int n) {
-        if (n <= 1) return n;
-        else return fibonacci(n - 1) + fibonacci(n - 2);
-    }
+    public int fibonacci(int n)  {
+        if (n < 2) return n;
 
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
 
 
